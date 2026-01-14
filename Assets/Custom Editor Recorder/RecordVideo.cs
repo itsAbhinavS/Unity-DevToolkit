c7a179e91b117a52c269fc29fc9116532efa2f00
Abhinav Satheesh <itsabhinavsatheesh@gmail.com>
2026-01-14 16:27:46 +0530

    Custom video recording settings - fps, resolution, output codec, encoding quality, file name and folder name

--- Assets/Custom Editor Recorder/RecordVideo.cs
+++ Assets/Custom Editor Recorder/RecordVideo.cs
@@ -1,6 +1,7 @@
 #if UNITY_EDITOR
 
 using System.IO;
+using Unity.VisualScripting;
 using UnityEditor.Recorder;
 using UnityEditor.Recorder.Encoder;
 using UnityEditor.Recorder.Input;
@@ -8,123 +9,196 @@ using UnityEngine.InputSystem;
 
 namespace UnityEngine.Recorder.Examples
 {
-    /// <summary>
-    /// This example shows how to set up a recording session via script.
-    /// To use this example, add the CaptureScreenShotExample component to a GameObject.
-    ///
-    /// Entering playmode will display a "Capture ScreenShot" button.
-    ///
-    /// Recorded images are saved in [Project Folder]/SampleRecordings
-    /// </summary>
     public class RecordVideo : MonoBehaviour
     {
-        public static RecordVideo Instance;
-
         RecorderController m_RecorderController;
-        public bool m_RecordAudio = false;
+
+        [Space(20)]
+        [Header("Video Recording Settings")]
+        [SerializeField] private Vector2 m_RecordResolution = new Vector2(1920, 1080);
+        [SerializeField] private bool m_RecordAudio = false;
+
+        [Space(20)]
+        [Header("Start Recording Input")]
+        [SerializeField] private InputAction startRecordingInput;
+        [Header("Stop Recording Input")]
+        [SerializeField] private InputAction stopRecordingInput;
+
+        [Space(20)]
+        [Header("Name of the output video file (without extension)")]
+        [SerializeField] private string outputFileName = "MyRecording";
+        
+        [Space(20)]
+        [Header("Output folder path relative to project root. Leave empty for default 'SampleRecordings'")]
+        [SerializeField] private string outputFolderPath = "SampleRecordings";
+        
+        [Space(20)]
+        [Header("Output codec format")]
+        [SerializeField] private CoreEncoderSettings.OutputCodec outputCodec = CoreEncoderSettings.OutputCodec.MP4;
+
+        [Space(20)]
+        [Header("Frame rate for the recording")]
+        [SerializeField] private float frameRate = 60.0f;
+
+        [Space(20)]
+        [Header("Video encoding quality")]
+        [SerializeField] private CoreEncoderSettings.VideoEncodingQuality encodingQuality = CoreEncoderSettings.VideoEncodingQuality.High;
+
         internal MovieRecorderSettings m_Settings = null;
-
-        private InputAction qKeyAction;
-        private InputAction eKeyAction;
-
-
-        private void Awake()
-        {
-            if (Instance == null)
-            {
-                Instance = this;
-                DontDestroyOnLoad(Instance);
-            }
-            else
-            {
-                Destroy(Instance);
-            }
-        }
 
         public FileInfo OutputFile
         {
             get
             {
-                var fileName = m_Settings.OutputFile + ".mp4";
+                var extension = GetFileExtension(outputCodec);
+                var fileName = m_Settings.OutputFile + extension;
                 return new FileInfo(fileName);
             }
         }
 
+
         private void OnEnable()
         {
-            // Q keyinput
-            qKeyAction = new InputAction("QKey", binding: "<Keyboard>/q");
-            qKeyAction.performed += OnQKeyPressed;
-            qKeyAction.Enable();
-
-            // E keyinput
-            eKeyAction = new InputAction("EKey", binding: "<Keyboard>/e");
-            eKeyAction.performed += OnEKeyPressed;
-            eKeyAction.Enable();
+            EnableRecordingInput();
         }
+        private void OnDisable()
+        {
+            DisableRecordingInput();
+        }
+
 
         internal void Initialize()
         {
             var controllerSettings = ScriptableObject.CreateInstance<RecorderControllerSettings>();
             m_RecorderController = new RecorderController(controllerSettings);
 
-            var mediaOutputFolder = new DirectoryInfo(Path.Combine(Application.dataPath, "..", "SampleRecordings"));
+            
+            string folderPath = string.IsNullOrEmpty(outputFolderPath) ? "SampleRecordings" : outputFolderPath;
+            var mediaOutputFolder = new DirectoryInfo(Path.Combine(Application.dataPath, "..", folderPath));
 
-            // Video
+            
+            // Create folder if it doesn't exist
+            if (!mediaOutputFolder.Exists)
+            {
+                mediaOutputFolder.Create();
+            }
+
+            
+            // Video settings
             m_Settings = ScriptableObject.CreateInstance<MovieRecorderSettings>();
             m_Settings.name = "My Video Recorder";
             m_Settings.Enabled = true;
 
-            // This example performs an MP4 recording
+            
+            // Encoder settings
             m_Settings.EncoderSettings = new CoreEncoderSettings
             {
-                EncodingQuality = CoreEncoderSettings.VideoEncodingQuality.High,
-                Codec = CoreEncoderSettings.OutputCodec.MP4
+                EncodingQuality = encodingQuality,
+                Codec = outputCodec
             };
             m_Settings.CaptureAlpha = true;
 
             m_Settings.ImageInputSettings = new GameViewInputSettings
             {
-                //OutputWidth = 1440,
-                //OutputHeight = 2560,
-                OutputWidth = 1080,
-                OutputHeight = 1920
+                OutputWidth = (int)m_RecordResolution.x,
+                OutputHeight = (int)m_RecordResolution.y
             };
 
-            // Simple file name (no wildcards) so that FileInfo constructor works in OutputFile getter.
-            m_Settings.OutputFile = mediaOutputFolder.FullName + "/" + "video";
+
+            // Output file path
+            int takeCount = PlayerPrefs.GetInt("VideoTakes", 0);
+            takeCount++;
+            PlayerPrefs.SetInt("VideoTakes", takeCount);
+            string take = takeCount.ToString();
+            m_Settings.OutputFile = $"{mediaOutputFolder.FullName}/{outputFileName}_Resolution{m_RecordResolution.x}x{m_RecordResolution.y}_Fps{frameRate}_Take{take}";
+
 
             // Setup Recording
             controllerSettings.AddRecorderSettings(m_Settings);
             controllerSettings.SetRecordModeToManual();
-            controllerSettings.FrameRate = 60.0f;
+            controllerSettings.FrameRate = frameRate;
 
             RecorderOptions.VerboseMode = false;
             m_RecorderController.PrepareRecording();
             m_RecorderController.StartRecording();
 
-            Debug.Log($"Started recording for file {OutputFile.FullName}");
+            Debug.Log($"STARTED RECORDING");
         }
 
-        private void OnDisable()
+
+
+        #region Start/Stop Recording
+        private void EnableRecordingInput()
         {
-            qKeyAction.Disable();
+            // START RECORDING
+            if (startRecordingInput == null || startRecordingInput.bindings.Count == 0)
+            {
+                startRecordingInput = new InputAction(
+                    "StartRecording",
+                    InputActionType.Button,
+                    "<Keyboard>/q"
+                );
+            }
+
+            startRecordingInput.performed += OnStartRecordPressed;
+            startRecordingInput.Enable();
+
+
+            // STOP RECORDING
+            if (stopRecordingInput == null || stopRecordingInput.bindings.Count == 0)
+            {
+                stopRecordingInput = new InputAction(
+                    "StopRecording",
+                    InputActionType.Button,
+                    "<Keyboard>/e"
+                );
+            }
+
+            stopRecordingInput.performed += OnStopRecordPressed;
+            stopRecordingInput.Enable();
         }
 
-        private void OnQKeyPressed(InputAction.CallbackContext context)
+        private void DisableRecordingInput()
+        {
+            if (startRecordingInput != null)
+            {
+                startRecordingInput.performed -= OnStartRecordPressed;
+                startRecordingInput.Disable();
+            }
+
+            if (stopRecordingInput != null)
+            {
+                stopRecordingInput.performed -= OnStopRecordPressed;
+                stopRecordingInput.Disable();
+            }
+        }
+        private void OnStartRecordPressed(InputAction.CallbackContext context)
         {
             Initialize();
         }
-        private void OnEKeyPressed(InputAction.CallbackContext context)
+        private void OnStopRecordPressed(InputAction.CallbackContext context)
         {
             m_RecorderController.StopRecording();
+            Debug.Log($"RECORDING SAVED TO : {OutputFile.FullName}");
         }
+        #endregion Start/Stop Recording
 
+
+
+        private string GetFileExtension(CoreEncoderSettings.OutputCodec codec)
+        {
+            switch (codec)
+            {
+                case CoreEncoderSettings.OutputCodec.MP4:
+                    return ".mp4";
+                case CoreEncoderSettings.OutputCodec.WEBM:
+                    return ".webm";
+                default:
+                    return ".mp4";
+            }
+        }
     }
 
 }
 
 #endif
-
-
-
